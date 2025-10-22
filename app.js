ad182dce1eeb1944614548829b5a12fd8d564c6e
Eswari <gokulaeswari06@gmail.com>
2025-10-22 15:45:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello Jenkins CI/CD!,This is Eswari from Elavate labs!!!'));
+app.get('/', (req, res) => res.send('Hello Jenkins CI/CD!,This is Eswari from Elavate labs - task2'));
 app.listen(3000, () => console.log('App running on port 3000'));
