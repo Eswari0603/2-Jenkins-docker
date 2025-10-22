160492ccbece1dd5abb7cbb8dafa44032d79fbe4
Eswari <gokulaeswari06@gmail.com>
2025-10-22 15:05:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello Jenkins CI/CD!,This is My task2 Jenksins demo app'));
+app.get('/', (req, res) => res.send('Hello Jenkins CI/CD!,This is My task2 Jenksins demo app!!!!'));
 app.listen(3000, () => console.log('App running on port 3000'));
